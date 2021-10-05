86c8115cf1e773a697f17998a7a2d8af9790d7d5
Esteban <encinaesteban27@gmail.com>
2021-10-05 09:45:49 -0300

    closeConnection, downloadAFileFromSFTPConnection

--- SFTP_to_MSSQL.py
+++ SFTP_to_MSSQL.py
@@ -36,19 +36,47 @@ def getCredentials(transport):
     except Exception as e: 
         print('Error, please review your credentials. ',e)
 
+def getFile(sftp, localpath, filepath):
+    #sftp.get(filepath, localpath)
+    pass
+def closeConnection(sftp, transport):
+    try:
+        if sftp:
+            sftp.close()
+        if transport:
+            transport.close()
+    except Exception as e:
+        print('Error. The sftp or transport service are not working well. Please, retry...', e)
+
 
 if __name__=='__main__':
     #Extract part
+    
     #Connect to SFTP
     paramiko.util.log_to_file("paramiko.log")
     # Open Connection
     transport = getTransport()
     # Connect to sftp with credentials
     sftp = getCredentials(transport)
+    #Download a file from a SFTP connection with a filepath and a localpath 
+    filepath = "/coto/upload/TEMPLATECOTO.xlsx"
+    localpath = "./templateCoto.xlsx"
+    getFile(sftp, filepath, localpath)
+    # Close the connection
+    closeConnection(sftp, transport)
+
     #Transformation part
+
     #We extract this from a SFTP but in this project we'll use an example xlsx file.
     df = pd.read_excel('datasets/templateCoto.xlsx', engine = 'openpyxl')
-    
+    df = df.rename(columns = {'idmorodo':'IDMOROSO', 'idcliente':'IDCLIENTE', 'Telefono':'TELEFONO',
+    'estado':'endpoint'})
+    df['fecha_carga'] = time.strftime("%Y-%d-%m %H:%M:%S", time.localtime())
+    df['estado_inicial'] = np.nan
+    df['tipo_bot'] = 'Coto'
+    df['fecha_interaccion'] = np.nan
+    df = df[['IDMOROSO', 'IDCLIENTE', 'TELEFONO','Saldo Minimo', 'Saldo Total', 'tipo_bot' ,'endpoint','fecha_carga']]
+
 
 
 
