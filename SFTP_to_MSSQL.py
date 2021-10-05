982cbb3f0b2687e36fe45a6a39818230a03f3be1
Esteban <encinaesteban27@gmail.com>
2021-10-05 13:42:01 -0300

    Transform and Load data into Database

--- SFTP_to_MSSQL.py
+++ SFTP_to_MSSQL.py
@@ -11,6 +11,8 @@ import getpass
 from paramiko import transport
 
 from paramiko.transport import Transport
+import pypyodbc as podbc
+import json
 
 def getTransport():
     while True:
@@ -48,10 +50,21 @@ def closeConnection(sftp, transport):
     except Exception as e:
         print('Error. The sftp or transport service are not working well. Please, retry...', e)
 
+def connectDB(username, ip,password, base = 'active'):
+    try:
+        server = ip
+        database = 'master'
+        username = username
+        password = password
+        cnxn = podbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+        return cnxn
+    except:
+        print("Connection with {} failed".format(base))
+
 
 if __name__=='__main__':
     #Extract part
-    
+
     #Connect to SFTP
     paramiko.util.log_to_file("paramiko.log")
     # Open Connection
@@ -67,7 +80,7 @@ if __name__=='__main__':
 
     #Transformation part
 
-    #We extract this from a SFTP but in this project we'll use an example xlsx file.
+    #We extract this from a SFTP but in this project we'll use a xlsx example file.
     df = pd.read_excel('datasets/templateCoto.xlsx', engine = 'openpyxl')
     df = df.rename(columns = {'idmorodo':'IDMOROSO', 'idcliente':'IDCLIENTE', 'Telefono':'TELEFONO',
     'estado':'endpoint'})
@@ -76,6 +89,32 @@ if __name__=='__main__':
     df['tipo_bot'] = 'Coto'
     df['fecha_interaccion'] = np.nan
     df = df[['IDMOROSO', 'IDCLIENTE', 'TELEFONO','Saldo Minimo', 'Saldo Total', 'tipo_bot' ,'endpoint','fecha_carga']]
+
+    #Getting credentials from a json to connect to DB.
+    f = open('credentials.json')
+    data = json.load(f)
+    username = data['username']
+    password = data['password']
+    ip = int(data['ip'])
+    cnxn = connectDB(base = 'warehouse', ip=ip, username=username, password=password)
+    #Working with connection and load data into database
+    cursor = cnxn.cursor()
+    for i in range(len(df)):
+        values = df.iloc[i, :].to_list()
+        query = f""" INSERT INTO DATA_WAREHOUSE.DBO.DAM_FRONEUS(IDMOROSO, IDCLIENTE, TELEFONO,SALDO_MINIMO, SALDO_TOTAL,
+        TIPO_BOT, ENDPOINT, FECHA_CARGA) VALUES {tuple(values)}
+        """
+        try:
+            cursor.execute(query)
+            cnxn.commit()
+            print('Insert Finish!')
+        except Exception as e: 
+            print("Error. Please review your query. ", e)
+    #Close connection
+    cnxn.close()
+
+
+
 
 
 
